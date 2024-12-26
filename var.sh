4e0764f99e2686df3a4b733435907387071ce206
vvnfrnds <119041296+vvnfrnds@users.noreply.github.com>
2024-12-26 13:08:47 +0400

    Create var.sh

---
+++ var.sh
@@ -0,0 +1,4 @@
+export TF_VAR_subscription_id="YOUR_SUBSCRIPTION_ID"
+export TF_VAR_client_id="YOUR_CLIENT_ID"
+export TF_VAR_client_secret="YOUR_CLIENT_SECRET"
+export TF_VAR_tenant_id="YOUR_TENANT_ID"
